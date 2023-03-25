549b50820ba56ce14e75d7a316c1f9c9f9cbfa72
UMESH PANCHAL <srzonea@gmail.com>
2023-03-25 15:42:42 +0530

    SECOND commit

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter two number:";
+    cin>>a>>b;
+    int dif;
+    dif=a-b;
+    cout<<"\nDIFF is:"<<dif;
+    cout<<"\nYOU diff IS ABOVE";
+    return 0;
+}
